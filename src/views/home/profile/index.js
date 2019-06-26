ddc23ee2594c9e7d6049c05c55e8798053419f26
Karl Chvojka <karl.chvojka@gmail.com>
2019-06-25 22:15:03 -0400

    Member profile api added

--- src/views/home/profile/index.js
+++ src/views/home/profile/index.js
@@ -1,38 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import ProfileImg from '../assets/amandaProfile.png';
 import ShoppingBagIcon from '../assets/shoppingBagIcon.png';
 import HandIcon from '../assets/handIcon.png';
 import CrownIcon from '../assets/crownIcon.png';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
-function Profile() {
-  return (
-    <Container as='section' className="colInner profileWrap">
-      <Row className="imageWrap">
-        <Col xl={12} lg={12} md={12} sm={12} xs={12}>
-          <img className="profileImg" src={ProfileImg} alt="Profile" />
-        </Col>
-      </Row>
-      <Row className="profileDetails">
-        <Col xl={12} lg={12} md={12} sm={12} xs={12}>
-          <h3>Amanda Bloomfield</h3>
-          <h4>28,549</h4>
-          <p>Points Available</p>
-        </Col>
-      </Row>
-      <Row className="profileOptions">
-        <Col xl={12} lg={12} md={12} sm={12} xs={12}>
-          <ul>
-            <li><p><img src={ShoppingBagIcon} alt="Shopping Bag" />Shop The Gallery</p></li>
-            <li><p><img src={HandIcon} alt="Hand with a leaf behind it" />Communications</p></li>
-            <li><p><img src={CrownIcon} alt="Crown" />Recognize Peers</p></li>
-          </ul>
-        </Col>
-      </Row>
-    </Container>
-  );
+
+class Profile extends Component {
+  render() {
+    return (
+      <Container as='section' className="colInner profileWrap">
+        <Row className="imageWrap">
+          <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+            <img className="profileImg" src={ProfileImg} alt="Profile" />
+          </Col>
+        </Row>
+        <Row className="profileDetails">
+          <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+            <h3>{this.props.user.first_name} {this.props.user.last_name}</h3>
+            <h4>{this.props.user.points}</h4>
+            <p>Points Available</p>
+          </Col>
+        </Row>
+        <Row className="profileOptions">
+          <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+            <ul>
+              <li><p><img src={ShoppingBagIcon} alt="Shopping Bag" />Shop The Gallery</p></li>
+              <li><p><img src={HandIcon} alt="Hand with a leaf behind it" />Communications</p></li>
+              <li><p><img src={CrownIcon} alt="Crown" />Recognize Peers</p></li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Profile;
